8745d4efc1e3cb90243e98d315aef22c60e0242d
joaoh1 <joaoh1@users.noreply.github.com>
2020-09-29 15:06:07 -0300

    Remove async

--- backend/triday_utils.py
+++ backend/triday_utils.py
@@ -1,10 +1,10 @@
 import math
 
-async def get_triday(day):
+def get_triday(day):
     triday = math.ceil(day / 3)
     return triday
 
-async def get_days_from_triday(triday):
+def get_days_from_triday(triday):
     triday *= 3
     days = [triday - 2, triday - 1, triday]
     return days
