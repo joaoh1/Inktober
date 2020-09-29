a83a8a710d26f02da5826f03bbb63b75094d39f5
joaoh1 <joaoh1@users.noreply.github.com>
2020-09-29 15:09:14 -0300

    Apply the suggestions

--- backend/sheets/sheets.py
+++ backend/sheets/sheets.py
@@ -46,15 +46,15 @@ class Sheets(commands.Cog):
         triday = backend.triday_utils.get_triday(now_day)
         triday_days = backend.triday_utils.get_days_from_triday(triday)
         if triday == 11 and now_date.month == self.ink_month:
-            topic = (f"Day **31**: {backend.day_themes.day_themes[now_day]}")
+            topic = f"Day **31**: {backend.day_themes.day_themes[now_day]}"
         elif now_day == triday_days[0] and now_date.month == self.ink_month:
-            topic = (f"Days **{triday_days[0]}**, {triday_days[1]} and {triday_days[2]}: {backend.day_themes.day_themes[now_day]}")
+            topic = f"Days **{triday_days[0]}**, {triday_days[1]} and {triday_days[2]}: {backend.day_themes.day_themes[now_day]}"
         elif now_day == triday_days[1] and now_date.month == self.ink_month:
-            topic = (f"Days {triday_days[0]}, **{triday_days[1]}** and {triday_days[2]}: {backend.day_themes.day_themes[now_day]}")
+            topic = f"Days {triday_days[0]}, **{triday_days[1]}** and {triday_days[2]}: {backend.day_themes.day_themes[now_day]}"
         elif now_day == triday_days[2] and now_date.month == self.ink_month:
-            topic = (f"Days {triday_days[0]}, {triday_days[1]} and **{triday_days[2]}**: {backend.day_themes.day_themes[now_day]}")
+            topic = f"Days {triday_days[0]}, {triday_days[1]} and **{triday_days[2]}**: {backend.day_themes.day_themes[now_day]}"
 
-        topic_str = f"Currently accepting " + topic if topic != "" else "Not accepting any submissions at this time"
+        topic_str = f"Currently accepting {topic}" if topic != "" else "Not accepting any submissions at this time"
         await channel.edit(
             reason="1 hr Time passed",
             topic=topic_str
